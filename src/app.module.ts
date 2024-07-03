bd68bfe12f99270cb12f35e77f387e3c7b7e0647
CodedSplash <shchukin.vania@gmail.com>
2024-07-03 18:50:31 +0300

    feat(Jwt): Создал модуль для работы с jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { ErrorHandlerFilter } from './application/filters/ErrorHandler.filter';
+import { JwtModule } from './core/Jwt/jwt.module';
 import { UserProfileModule } from './core/UserProfile/userProfile.module';
 import { DbModule } from './infrastructure/db/db.module';
 
@@ -10,6 +11,7 @@ import { DbModule } from './infrastructure/db/db.module';
     DbModule,
     ConfigModule.forRoot({ isGlobal: true }),
     UserProfileModule,
+    JwtModule,
   ],
   providers: [
     {
