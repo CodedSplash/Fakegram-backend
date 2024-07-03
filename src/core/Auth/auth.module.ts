ac8f104cc4973ace1778142cbf7e8e50ef9dc531
CodedSplash <shchukin.vania@gmail.com>
2024-07-03 20:07:51 +0300

    feat(Auth): Импортировал JwtModule и UserProfileModule в Auth модуль

--- src/core/Auth/auth.module.ts
+++ src/core/Auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserAuthRepository } from '../../infrastructure/repositories/Auth/userAuth.repository';
+import { JwtModule } from '../Jwt/jwt.module';
+import { UserProfileModule } from '../UserProfile/userProfile.module';
 
 @Module({
+  imports: [JwtModule, UserProfileModule],
   providers: [UserAuthRepository],
 })
 export class AuthModule {}
