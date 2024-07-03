a35191ec76508cf3f743953e526c85d135f21a48
CodedSplash <shchukin.vania@gmail.com>
2024-07-03 19:12:40 +0300

    feat(Jwt): Импортировал UserProfileModule в Jwt модуль

--- src/core/Jwt/jwt.module.ts
+++ src/core/Jwt/jwt.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RefreshJwtTokenRepository } from '../../infrastructure/repositories/Jwt/refreshJwtToken.repository';
+import { UserProfileModule } from '../UserProfile/userProfile.module';
 
 @Module({
+  imports: [UserProfileModule],
   providers: [RefreshJwtTokenRepository],
 })
 export class JwtModule {}
