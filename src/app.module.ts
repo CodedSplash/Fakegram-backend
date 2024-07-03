d2bce2b1cd56c8d3f06c99beb89319e3e2bc5861
CodedSplash <shchukin.vania@gmail.com>
2024-07-03 19:54:28 +0300

    feat(Auth): Добавил модуль Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { ErrorHandlerFilter } from './application/filters/ErrorHandler.filter';
+import { AuthModule } from './core/Auth/auth.module';
 import { JwtModule } from './core/Jwt/jwt.module';
 import { UserProfileModule } from './core/UserProfile/userProfile.module';
 import { DbModule } from './infrastructure/db/db.module';
@@ -12,6 +13,7 @@ import { DbModule } from './infrastructure/db/db.module';
     ConfigModule.forRoot({ isGlobal: true }),
     UserProfileModule,
     JwtModule,
+    AuthModule,
   ],
   providers: [
     {
