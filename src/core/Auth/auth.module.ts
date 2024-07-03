18f199bdbed0530bd9c15ac97f935c767067c728
CodedSplash <shchukin.vania@gmail.com>
2024-07-03 20:09:23 +0300

    feat(service/Auth): Добавил сервис для Auth модуля

--- src/core/Auth/auth.module.ts
+++ src/core/Auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserAuthRepository } from '../../infrastructure/repositories/Auth/userAuth.repository';
 import { JwtModule } from '../Jwt/jwt.module';
 import { UserProfileModule } from '../UserProfile/userProfile.module';
+import { UserAuthService } from './servicies/userAuth.service';
 
 @Module({
   imports: [JwtModule, UserProfileModule],
-  providers: [UserAuthRepository],
+  providers: [UserAuthRepository, UserAuthService],
 })
 export class AuthModule {}
