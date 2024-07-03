66f0463a0794b2ccbae3a030b156c80dac72a5a6
CodedSplash <shchukin.vania@gmail.com>
2024-07-03 20:12:24 +0300

    feat(controller/Auth): Добавил контроллер для Auth модуля

---
+++ src/application/controllers/Auth/userAuth.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Inject, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { UserAuthService } from '../../../core/Auth/servicies/userAuth.service';
+import { IUserAuthService } from '../../../core/Auth/servicies/userAuth.service.interface';
+import { UserRegistrationDto } from '../../dtos/Auth/userRegistration.dto';
+import { UserResponseDto } from '../../dtos/Auth/userResponse.dto';
+
+@Controller('auth')
+export class UserAuthController {
+  constructor(
+    @Inject(UserAuthService) private readonly userAuthService: IUserAuthService,
+  ) {}
+
+  @Post('registration')
+  async registration(
+    @Body() dto: UserRegistrationDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { user, jwt } = await this.userAuthService.registration(
+      new UserRegistrationDto(dto),
+    );
+
+    res.cookie('refreshToken', jwt.refreshToken, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+    return new UserResponseDto(user);
+  }
+}
--- src/core/Auth/auth.module.ts
+++ src/core/Auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { UserAuthController } from '../../application/controllers/Auth/userAuth.controller';
 import { UserAuthRepository } from '../../infrastructure/repositories/Auth/userAuth.repository';
 import { JwtModule } from '../Jwt/jwt.module';
 import { UserProfileModule } from '../UserProfile/userProfile.module';
@@ -7,5 +8,6 @@ import { UserAuthService } from './servicies/userAuth.service';
 @Module({
   imports: [JwtModule, UserProfileModule],
   providers: [UserAuthRepository, UserAuthService],
+  controllers: [UserAuthController],
 })
 export class AuthModule {}
