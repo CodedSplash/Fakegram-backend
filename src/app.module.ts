63712e7033c8526ba22de66b1aaf55916081a881
CodedSplash <shchukin.vania@gmail.com>
2024-07-03 11:10:54 +0300

    feat(UserProfile): Создал UserProfile модуль

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { ErrorHandlerFilter } from './application/filters/ErrorHandler.filter';
+import { UserProfileModule } from './core/UserProfile/userProfile.module';
 import { DbModule } from './infrastructure/db/db.module';
 
 @Module({
   imports: [
     DbModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    UserProfileModule,
   ],
   providers: [
     {
