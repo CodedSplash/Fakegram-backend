ad0970a9eca8301dc796e6bf6a3654508e2af300
CodedSplash <shchukin.vania@gmail.com>
2024-07-12 17:04:01 +0300

    feat(dto/UserProfile): Добавил документацию для HasUserResponseDto

--- src/application/dtos/UserProfile/hasUserResponse.dto.ts
+++ src/application/dtos/UserProfile/hasUserResponse.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { GenerateDtoParameterType } from '../../../common/types/dtoParameter.type';
 
 export class HasUserResponseDto {
+  @ApiProperty()
   readonly username: string;
 
   constructor(user: GenerateDtoParameterType<HasUserResponseDto>) {
