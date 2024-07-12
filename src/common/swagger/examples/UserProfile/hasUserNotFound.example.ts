803ac28963e4f3e578e6662c22291fb65106cb7e
CodedSplash <shchukin.vania@gmail.com>
2024-07-12 17:07:00 +0300

    feat(example/UserProfile): Создал примеры ответов для документации UserProfileController

---
+++ src/common/swagger/examples/UserProfile/hasUserNotFound.example.ts
@@ -0,0 +1,9 @@
+export const hasUserNotFoundExample = {
+  status: 404,
+  error: {
+    message: 'Пользователь не найден!',
+    error: 'Not Found',
+    statusCode: 404,
+  },
+  path: '/user/has_user/test_1',
+};
