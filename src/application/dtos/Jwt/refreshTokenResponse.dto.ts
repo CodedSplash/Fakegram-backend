a9f36a76f5eb77e9815add46adfb298bc4e158b5
CodedSplash <shchukin.vania@gmail.com>
2024-07-12 19:41:06 +0300

    feat(dto/Jwt): Добавил документацию в RefreshTokenResponseDto

--- src/application/dtos/Jwt/refreshTokenResponse.dto.ts
+++ src/application/dtos/Jwt/refreshTokenResponse.dto.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { GenerateDtoParameterType } from '../../../common/types/dtoParameter.type';
 
 export class RefreshTokenResponseDto {
+  @ApiProperty()
   readonly accessToken: string;
+
+  @ApiProperty()
   readonly refreshToken: string;
 
   constructor(tokens: GenerateDtoParameterType<RefreshTokenResponseDto>) {
