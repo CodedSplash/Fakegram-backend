2759ec85423764a08e2e607ebe4ad8b3dfee4f8e
CodedSplash <shchukin.vania@gmail.com>
2024-07-12 16:29:36 +0300

    feat(example/Auth): Создал примеры ответов с сервера для документации

---
+++ src/common/swagger/examples/Auth/userRegistrationBadRequest.example.ts
@@ -0,0 +1,46 @@
+import { getSchemaPath } from '@nestjs/swagger';
+import { DefaultErrorResponseType } from '../../../types/defaultErrorResponse.type';
+import { ValidationErrorResponseType } from '../../../types/validationErrorResponse.type';
+
+const userAlreadyRegisteredExample = {
+  summary: 'Пользователь уже зарегистрирован',
+  value: {
+    status: 400,
+    error: {
+      message: 'Пользователь уже зарегистрирован',
+      error: 'Bad Request',
+      statusCode: 400,
+    },
+    path: '/auth/registration/',
+  },
+};
+
+const validationErrorExample = {
+  summary: 'Ошибка при валидации данных',
+  value: {
+    status: 400,
+    error: {
+      message: [
+        'Минимальная длина поля username должна составлять не менее 6 символов',
+      ],
+      error: 'Bad Request',
+      statusCode: 400,
+    },
+    path: '/auth/registration/',
+  },
+};
+
+export const userRegistrationBadRequestExample = {
+  'application/json': {
+    schema: {
+      oneOf: [
+        { $ref: getSchemaPath(DefaultErrorResponseType) },
+        { $ref: getSchemaPath(ValidationErrorResponseType) },
+      ],
+    },
+    examples: {
+      userAlreadyRegisteredExample,
+      validationErrorExample,
+    },
+  },
+};
