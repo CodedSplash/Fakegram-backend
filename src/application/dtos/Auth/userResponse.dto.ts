f3aaa8d606bc5c11a0248b7e54bafe0b62dd024e
CodedSplash <shchukin.vania@gmail.com>
2024-07-12 15:32:21 +0300

    feat(dto/Auth): Добавил документацию ответа после регистрации

--- src/application/dtos/Auth/userResponse.dto.ts
+++ src/application/dtos/Auth/userResponse.dto.ts
@@ -1,16 +1,54 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 import { GenerateDtoParameterType } from '../../../common/types/dtoParameter.type';
 
 export class UserResponseDto {
+  @ApiProperty()
   readonly id: number;
+
+  @ApiProperty({
+    required: false,
+  })
   readonly name?: string;
+
+  @ApiProperty()
   readonly username: string;
+
+  @ApiProperty({
+    required: false,
+    nullable: true,
+  })
   readonly country?: string;
+
+  @ApiProperty()
   readonly profilePhotoUrl?: string;
+
+  @ApiProperty({
+    enum: Role,
+    enumName: 'Role',
+    isArray: true,
+  })
   readonly roles: Role[];
+
+  @ApiProperty({
+    required: false,
+    nullable: true,
+  })
   readonly aboutMe?: string;
+
+  @ApiProperty({
+    required: false,
+  })
   readonly registrationDate?: Date;
+
+  @ApiProperty({
+    required: false,
+  })
   readonly isVerified?: boolean;
+
+  @ApiProperty({
+    required: false,
+  })
   readonly isPrivate?: boolean;
 
   constructor(user: GenerateDtoParameterType<UserResponseDto>) {
