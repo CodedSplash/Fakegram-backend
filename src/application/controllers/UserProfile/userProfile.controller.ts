f90e5a562cb3fc4254be01d33e1421230e49aeff
CodedSplash <shchukin.vania@gmail.com>
2024-07-12 17:07:55 +0300

    feat(controller/UserProfile): Добавил документацию для UserProfileController

--- src/application/controllers/UserProfile/userProfile.controller.ts
+++ src/application/controllers/UserProfile/userProfile.controller.ts
@@ -1,9 +1,20 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { internalServerErrorExample } from '../../../common/swagger/examples/general/internalServerError.example';
+import { hasUserNotFoundExample } from '../../../common/swagger/examples/UserProfile/hasUserNotFound.example';
+import { hasUserResponseExample } from '../../../common/swagger/examples/UserProfile/hasUserResponse.example';
+import { DefaultErrorResponseType } from '../../../common/types/defaultErrorResponse.type';
 import { UserProfileService } from '../../../core/UserProfile/servicies/userProfile.service';
 import { IUserProfileService } from '../../../core/UserProfile/servicies/userProfile.service.interface';
 import { HasUserResponseDto } from '../../dtos/UserProfile/hasUserResponse.dto';
 
 @Controller('user')
+@ApiTags('User profile')
 export class UserProfileController {
   constructor(
     @Inject(UserProfileService)
@@ -11,6 +22,21 @@ export class UserProfileController {
   ) {}
 
   @Get('has_user/:username')
+  @ApiOkResponse({
+    description: 'Пользователь найден!',
+    type: HasUserResponseDto,
+    example: hasUserResponseExample,
+  })
+  @ApiNotFoundResponse({
+    description: 'Пользователь не найден!',
+    type: DefaultErrorResponseType,
+    example: hasUserNotFoundExample,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Ошибка на стороне сервера!',
+    type: DefaultErrorResponseType,
+    example: internalServerErrorExample('/user/has_user/test_1'),
+  })
   async hasUser(@Param('username') username: string) {
     const user = await this.userProfileService.hasUser(username);
 
